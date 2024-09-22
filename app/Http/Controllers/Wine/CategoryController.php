631db0008d953f6b6cc19a69ece61a9ad7ba4080
Oscar <DeveloperMAC2023@gmail.com>
2024-09-22 15:29:48 -0300

    Obtener categorias

--- app/Http/Controllers/Wine/CategoryController.php
+++ app/Http/Controllers/Wine/CategoryController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Wine;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use Illuminate\Contracts\View\View;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function __construct(private readonly CategoryRepositoryInterface $repository)
+    {
+        
+    }
+
+    public function index(): View
+    {
+        return view('wine.category.index', [
+            'categories' => $this->repository->paginate(
+                counts: ['wines'],
+            )
+        ]);
+    }
 }
