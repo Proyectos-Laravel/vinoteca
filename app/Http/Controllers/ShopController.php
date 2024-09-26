b76239bab1d64b08f4099ec2d71d9ca7430de95d
Oscar <DeveloperMAC2023@gmail.com>
2024-09-26 12:04:52 -0300

    Datos de la tienda

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Cart;
 use Illuminate\Http\Request;
+use App\Traits\Traits\CartActions;
+use App\Repositories\Shop\ShopRepositoryInterface;
+use Illuminate\Contracts\View\View;
 
 class ShopController extends Controller
 {
-    //
+    
+    use CartActions;
+    
+    public function __construct(private readonly ShopRepositoryInterface $repository, private readonly Cart $cart)    
+    {
+        
+    }
+
+    public function index(): View
+    {
+        $wines = $this->repository->paginate();
+
+        return view('shop.index', compact('wines'));
+    }
 }
